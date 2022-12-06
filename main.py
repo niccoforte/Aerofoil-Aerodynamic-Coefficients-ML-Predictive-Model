c8acdd0c9e6942e7f6a668d4f636c0f1df70deb1
niccoforte <niccolo_forte@yahoo.it>
2022-12-06 20:21:33 +0000

    Major fix - error files decreased from ~650 to 53; Cleanup.

--- main.py
+++ main.py
@@ -4,52 +4,9 @@ import pandas as pd
 import profile
 
 
-def plot_profile(df, indx, x_val=None):
-    """Plots the profile of a given aerofoil.
-
-    Parameters
-    ----------
-    df : pd.DataFrame
-        Dataframe containing the aerofoils' data.
-    indx : int
-        Index of the aerofoil to be plotted.
-    x_val : float, optional
-        x-value to be plotted along with the aerofoil.
-    """
-
-    print(f'Plotting {df.name[indx]} Aerofoil...')
-
-    plt.figure(1)
-    plt.title(df.name[indx])
-    plt.plot(df.x[indx], df.y_low[indx])
-    plt.plot(df.x[indx], df.y_up[indx])
-    plt.ylim([min(df.y_low[indx]) - 0.15, max(df.y_up[indx]) + 0.15])
-    plt.grid()
-
-    if x_val:
-        print(f' and evaluating y coordinates at x={x_val}...')
-
-        splines = df.spline[indx]
-        spline_up = splines[0]
-        spline_low = splines[1]
-
-        y_val_up = spline_up(x_val)
-        y_val_low = spline_low(x_val)
-        print(f'  y_up = {y_val_up.round(5)} and y_low = {y_val_low.round(5)}.')
-
-        plt.figure(1)
-        plt.plot(x_val, y_val_up, marker='o', color='orangered')
-        plt.plot(x_val, y_val_low, marker='o', color='orangered')
-        plt.axvline(x=x_val, color='orangered')
-
-    plt.show()
-
-
-# ------------------------ Start Main ------------------------
-
-# Create list of profile objects
+# Create list and dataframe of profile objects
 profiles, aerofoils_df = profile.create_profiles()
 
-worked = aerofoils_df[aerofoils_df['x'] != 'Error'].reset_index()
-plot_profile(worked, 204, x_val=0.36)
-# print(len(worked.name.tolist()), worked.name.tolist())
+worked = aerofoils_df[aerofoils_df['x'] == 'Error'].reset_index()
+# profile.plot_profile(worked, 726, x_val=0.36)
+print(len(worked.name.tolist()), worked.name.tolist())
--- profile.py
+++ profile.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math
 import scipy.interpolate as interp
+from matplotlib import pyplot as plt
 import pandas as pd
 import os
 
@@ -30,6 +31,47 @@ def create_profiles(directory='aerofoil_dat', ext='dat', points=51, prnt=False):
             profiles.append(Profile(file, points=points, prnt=prnt))
 
     return profiles, Profile.dataframe
+
+
+def plot_profile(df, indx, x_val=None):
+    """Plots the profile of a given aerofoil.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Dataframe containing the aerofoils' data.
+    indx : int
+        Index of the aerofoil to be plotted.
+    x_val : float, optional
+        x-value to be plotted along with the aerofoil.
+    """
+
+    print(f'Plotting {df.name[indx]} Aerofoil...')
+
+    plt.figure(1)
+    plt.title(df.name[indx])
+    plt.plot(df.x[indx], df.y_low[indx])
+    plt.plot(df.x[indx], df.y_up[indx])
+    plt.ylim([min(df.y_low[indx]) - 0.15, max(df.y_up[indx]) + 0.15])
+    plt.grid()
+
+    if x_val:
+        print(f' and evaluating y coordinates at x={x_val}...')
+
+        splines = df.spline[indx]
+        spline_up = splines[0]
+        spline_low = splines[1]
+
+        y_val_up = spline_up(x_val)
+        y_val_low = spline_low(x_val)
+        print(f'  y_up = {y_val_up.round(5)} and y_low = {y_val_low.round(5)}.')
+
+        plt.figure(1)
+        plt.plot(x_val, y_val_up, marker='o', color='orangered')
+        plt.plot(x_val, y_val_low, marker='o', color='orangered')
+        plt.axvline(x=x_val, color='orangered')
+
+    plt.show()
 
 
 class Profile:
@@ -104,15 +146,20 @@ class Profile:
             empty_line_indxs = []
             indx = 0
             for line in lines:
-                indx += 1
                 line = line.strip()
-                if line == '': empty_line_indxs.append(indx - 1)
+                if line == '': empty_line_indxs.append(indx)
                 dat.append(line)
+                indx += 1
 
         # Set aerofoil name
-        if dat[0][-8:] == ' AIRFOIL': name = dat[0][0:-8]
-        else: name = dat[0]
-        dat = dat[empty_line_indxs[0] + 1:]
+        if dat[0][-8:] == ' AIRFOIL':
+            name = dat[0][0:-8]
+        else:
+            name = dat[0]
+        if dat[1] == '' or dat[2] == '':
+            dat = dat[empty_line_indxs[0] + 1:]
+        else:
+            dat = dat[1:]
 
         # Float dat list
         dat_flt = []
@@ -121,17 +168,37 @@ class Profile:
             line = [float(pt) for pt in line]
             dat_flt.append(line)
 
-        coords_up = dat_flt[:empty_line_indxs[1] - empty_line_indxs[0] - 1]
-        xs_up = [coord[0] for coord in coords_up]
-        ys_up = [coord[1] for coord in coords_up]
+        if len(empty_line_indxs) == 0 or len(empty_line_indxs) == 1:
+            xs = [coord[0] for coord in dat_flt]
+            ys = [coord[1] for coord in dat_flt]
+            indx = 1
+            for x in xs[1:]:
+                d = xs[indx + 1] - x
+                if d > 0:
+                    xmin_indx = indx
+                    xs.insert(xmin_indx, x)
+                    ys.insert(xmin_indx, ys[xmin_indx])
+                    break
+                indx += 1
+            xs_up = xs[:xmin_indx + 1]
+            xs_low = xs[xmin_indx + 1:]
+            ys_up = ys[:xmin_indx + 1]
+            ys_low = ys[xmin_indx + 1:]
+
+        else:
+            coords_up = dat_flt[:empty_line_indxs[1] - empty_line_indxs[0] - 1]
+            xs_up = [coord[0] for coord in coords_up]
+            ys_up = [coord[1] for coord in coords_up]
+
+            coords_low = dat_flt[empty_line_indxs[1] - empty_line_indxs[0]:]
+            xs_low = [coord[0] for coord in coords_low]
+            ys_low = [coord[1] for coord in coords_low]
+
+        # Sort upper and lower in ascending x
         sorter = pd.DataFrame({'x': xs_up, 'y': ys_up})
         sorter = sorter.sort_values('x')
         xs_up = list(sorter.x)
         ys_up = list(sorter.y)
-
-        coords_low = dat_flt[empty_line_indxs[1] - empty_line_indxs[0]:]
-        xs_low = [coord[0] for coord in coords_low]
-        ys_low = [coord[1] for coord in coords_low]
         sorter = pd.DataFrame({'x': xs_low, 'y': ys_low})
         sorter = sorter.sort_values('x')
         xs_low = list(sorter.x)
@@ -155,7 +222,6 @@ class Profile:
         self.coords_low = coords_low
         self.x = x
         self.y = y
-
         return name, coords_up, coords_low, x, y
 
     def x_distribution(self):
@@ -179,5 +245,4 @@ class Profile:
 
         self.spline_funcs = spline_funcs
         self.splines = splines
-
         return splines, spline_funcs
