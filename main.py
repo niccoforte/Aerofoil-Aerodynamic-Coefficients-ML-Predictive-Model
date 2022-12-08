41e5da45dc690b66a3a54541744678dd6c0bf8cb
niccoforte <niccolo_forte@yahoo.it>
2022-12-08 19:54:17 +0000

    Added random plotting to evaluate accuracy of multiple results faster. Plots 10 aerofoils one after another.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import random
 import profile
 
 # Download aerofoil .dat files to 'aerofoil_dat' directory
@@ -12,6 +13,12 @@ not_worked = aerofoils_df[aerofoils_df['x'] == 'Error']
 
 # pd.set_option('display.max_columns', None)
 # print(worked)
-profile.plot_profile(worked, 61, scatt=True, x_val=None, pltfig=1)
+
+# profile.plot_profile(worked, 61, scatt=True, x_val=None, pltfig=1)
+
+for i in range(10):
+    r = random.randint(0, len(worked))
+    profile.plot_profile(worked, r, scatt=True, x_val=None, pltfig=i)
+    print(r)
 
 # print(len(not_worked.name.tolist()), not_worked.name.tolist())
