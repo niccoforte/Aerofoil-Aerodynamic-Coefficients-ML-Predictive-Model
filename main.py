55d7be896aa0920cc8f7fd9402484e2065a7cda2
niccoforte <niccolo_forte@yahoo.it>
2022-12-10 20:13:53 +0000

    Changed layout to remove some redundant code. Avoided the `worked` and `not_worked` dfs thanks to change in dataframe structure (no "Error" rows). Included commented visualisation methods to print desired index from dataframe or randomly print 10.

--- main.py
+++ main.py
@@ -8,17 +8,12 @@ import profile
 # Create list and dataframe of profile objects
 profiles, aerofoils_df = profile.create_profiles()
 
-worked = aerofoils_df[aerofoils_df['x'] != 'Error']
-not_worked = aerofoils_df[aerofoils_df['x'] == 'Error']
+pd.set_option('display.max_columns', None)
+print(aerofoils_df)
 
-# pd.set_option('display.max_columns', None)
-# print(worked)
+# profile.plot_profile(aerofoils_df, 946, scatt=True, x_val=None, pltfig=1)
 
-# profile.plot_profile(worked, 61, scatt=True, x_val=None, pltfig=1)
-
-for i in range(10):
-    r = random.randint(0, len(worked))
-    profile.plot_profile(worked, r, scatt=True, x_val=None, pltfig=i)
-    print(r)
-
-# print(len(not_worked.name.tolist()), not_worked.name.tolist())
+# for i in range(10):
+#     r = random.randint(0, len(aerofoils_df))
+#     profile.plot_profile(aerofoils_df, r, scatt=True, x_val=None, pltfig=i)
+#     print(r)
